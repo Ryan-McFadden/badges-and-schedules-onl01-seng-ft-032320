30ee1dfdb2bb25946a6800bedc38213c06800154
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-25 21:27:21 +0000

    Automatically backed up by Learn

---
+++ conference_badges.rb
@@ -0,0 +1,14 @@
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |badge|
+    "Here's your badge #{badge}"
+  end
+end
+
+def assign_rooms(attendees)
+  attendees.each do |rooms|
+  end
+end
