b8c36577c0c3022dbe4c74762879617601e4ad39
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-03-25 21:38:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,15 @@
 def badge_maker(name)
-  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   attendees.each do |badge|
-    puts "Here's your badge #{badge}"
+   "Here's your badge #{badge}"
   end
 end
 
 def assign_rooms(attendees)
   attendees.each do |rooms|
-    "Hello, #{rooms}! You'll be assigned to room"
+    "Hello, #{rooms}! You'll be assigned to room #{room.index}"
   end
 end
